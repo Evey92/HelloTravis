10f2cd27413d275672c747a50625421bf48ad714
Ivan <evey9207@gmail.com>
2018-11-01 16:07:29 -0600

    aaaaaaaaaaaah

--- gtest.cmake
+++ gtest.cmake
@@ -1,4 +1,4 @@
-set(GOOGLETEST_ROOT TravisTest/externals/googletest CACHE STRING "Google Test source Root")
+set(GOOGLETEST_ROOT externals/googletest CACHE STRING "Google Test source Root")
 
 include_directories(SYSTEM
     ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}
