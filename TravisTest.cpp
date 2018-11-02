e2eb36c0c068666365fd2788ea4bf28c312df059
Ivan <evey9207@gmail.com>
2018-11-01 20:41:36 -0600

    cdc

--- TravisTest.cpp
+++ TravisTest.cpp
@@ -1,6 +1,59 @@
-#include <stdio.h>
-int main()
+#include<iostream>
+#include <gtest/gtest.h>
+#include <conio.h>
+
+class TMath
 {
-  printf("Hello World\n");
+public:
+  TMath() = default;
+  ~TMath() {};
+
+  float suma(float num1, float num2)
+  {
+    return num1 + num2;
+  }
+
+  float resta(float num1, float num2)
+  {
+    return num1 - num2;
+  }
+
+  float multi(float num1, float num2)
+  {
+    return num1 * num2;
+  }
+
+  float divi(float num1, float num2)
+  {
+    return num1 / num2;
+  }
+
+};
+
+TMath mmath;
+
+int main(int argc, char *argv[]) {
+  std::cout << "Hello World!" << std::endl;
+  ::testing::InitGoogleTest(&argc, argv);
+  std::cout << RUN_ALL_TESTS();
+  _getch();
+
   return 0;
+}
+
+TEST(Sum, Basic_Test)
+{
+
+  EXPECT_EQ(mmath.suma(2, 4), 6);
+  EXPECT_EQ(mmath.resta(9, 3), 6);
+}
+
+TEST(Multiplication, Basic_Test)
+{
+  EXPECT_EQ(mmath.multi(2, 4), 8);
+}
+
+TEST(Division, Basic_Test)
+{
+  EXPECT_EQ(mmath.divi(8, 2), 4);
 }
